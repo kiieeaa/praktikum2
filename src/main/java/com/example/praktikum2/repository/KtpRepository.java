692c1d3a353671be8ec6aa7914ffaa1ebfed8117
kiieeaa <kiyaadinda5@gmail.com>
2026-03-09 15:00:17 +0700

    digunakan untuk menghubungkan aplikasi dengan database dan menyediakan operasi CRUD secara otomatis melalui JpaRepository.

---
+++ src/main/java/com/example/praktikum2/repository/KtpRepository.java
@@ -0,0 +1,8 @@
+package com.example.praktikum2.repository;
+
+import com.example.praktikum2.model.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
+}
