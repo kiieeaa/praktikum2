b9dac82243e6ab9abf33915cf9f6c94225d8a3bd
kiieeaa <kiyaadinda5@gmail.com>
2026-03-09 14:51:44 +0700

    Menandakan bahwa class ini adalah Service yang berisi logika bisnis aplikasi

---
+++ src/main/java/com/example/praktikum2/service/KtpService.java
@@ -0,0 +1,10 @@
+package com.example.praktikum2.service;
+
+import com.example.praktikum2.model.Ktp;
+import com.example.praktikum2.repository.KtpRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class KtpService {
